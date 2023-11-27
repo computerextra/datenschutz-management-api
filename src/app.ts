881eb2f0d45e933eb29b858299be6c9bf1b8f987
Johannes Kirchner (Computer Extra) <service@computer-extra.de>
2023-11-27 11:55:36 +0100

    added body limit

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
+import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import cors from "cors";
 
-import * as middlewares from "./middlewares";
 import api from "./api";
 import MessageResponse from "./interfaces/MessageResponse";
+import * as middlewares from "./middlewares";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "200mb" }));
 
 app.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
